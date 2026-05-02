c3d38577a2b0b5f1b1831f0dab46e732bd3c8de7
deekshitha27980 <deekshithapatibandla71@gmail.com>
2026-05-02 14:35:44 +0530

    Move hibernate-delivery from backend repo into separate fsad-end-sem project and ignore it

--- hibernate-delivery/create-db.sql
+++
@@ -1,7 +0,0 @@
--- Create the database for the Hibernate Delivery HQL demo
-CREATE DATABASE fsadendexam;
-
--- Optional: grant privileges if you want a dedicated user
--- CREATE USER 'fsaduser'@'localhost' IDENTIFIED BY 'yourpassword';
--- GRANT ALL PRIVILEGES ON fsadendexam.* TO 'fsaduser'@'localhost';
--- FLUSH PRIVILEGES;
